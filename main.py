4fb48c82d6222b8c38e57370b175a44e14ca1925
Adrian Mace <5071859+adrianmace@users.noreply.github.com>
2025-08-01 17:32:05 +1000

    feat: better message format

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-def is_member_joined(before, after):
-    if before.channel == None and after.channel != None:
+def is_member_joined(before: discord.VoiceState, after: discord.VoiceState):
+    if before.channel is None and not after.channel is None:
         return True
     return False
 
@@ -19,26 +19,35 @@ async def on_ready():
     logger.info(f'We have logged in as {client.user}')
 
 @client.event
-async def on_voice_state_update(member, before, after):
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     logger = logging.getLogger('discord')
     if is_member_joined(before, after):
         destination = await client.fetch_channel(os.getenv('HYPERSCOUT_DESTINATION_CHANNEL_ID'))
 
         messages = [
-            "is now hanging out in",
-            "has joined",
-            "is chilling in",
-            "jumped into",
-            "entered",
+            "stumbled into",
+            "crash-landed in",
+            "respawned at",
+            "accidentally joined",
+            "teleported awkwardly into",
             "is vibing in",
-            "is now in"
+            "materialized suspiciously in",
+            "snuck into",
+            "rolled into",
+            "phased into existence in",
+            "just glitched into",
+            "tripped and fell into",
+            "is kicking back in",
+            "yeeted themselves into",
+            "opened a portal and walked into"
         ]
+
         message_text = random.choice(messages)
 
-        final_message = f'{member.display_name} {message_text} {after.channel.name}!'
+        final_message = f'{member.mention} {message_text} {after.channel.mention}!'
         logger.info(final_message)
 
-        await destination.send(final_message, delete_after=3600)
+        await destination.send(final_message, allowed_mentions=discord.AllowedMentions.none())
 
 if __name__ == "__main__":
     client.run(os.getenv('HYPERSCOUT_BOT_TOKEN'))
